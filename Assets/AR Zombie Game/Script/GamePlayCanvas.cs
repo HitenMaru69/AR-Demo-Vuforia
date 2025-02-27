9b5ac39eb8ab7034ee4cf18e37323a2da4a11191
Hiten <hitenmaru2000@gmail.com>
2025-02-27 19:33:49 +0530

    Basic Player functionality Done

--- Assets/AR Zombie Game/Script/GamePlayCanvas.cs
+++ Assets/AR Zombie Game/Script/GamePlayCanvas.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class GamePlayCanvas : MonoBehaviour
 {
     [SerializeField] PlayerAnimation playerAnimation;
-    
+    [SerializeField] MeshRenderer meshRenderer;
+
+
+    private void Update()
+    {
+        meshRenderer.enabled = false;
+    }
+
     public void AttackButton()
     {
         playerAnimation.PlayAttackAnimation();
--- Assets/AR Zombie Game/Script/PlaceObject.cs
+++ Assets/AR Zombie Game/Script/PlaceObject.cs
@@ -8,12 +8,16 @@ public class PlaceObject : MonoBehaviour
     [SerializeField] private bool isPlaced;
     [SerializeField] private GameObject planeFinder;
     [SerializeField] Player player;
+    [SerializeField] GameObject gamePlayCanvas;
+ 
    
 
     private void Start()
     {
         isPlaced = false;
+        gamePlayCanvas.SetActive(false);
     }
+
 
     public void PlaceContent(Transform content)
     {
@@ -21,6 +25,7 @@ public class PlaceObject : MonoBehaviour
         {
             isPlaced = true;
             player.SetToRigidbodyNormal();
+            gamePlayCanvas.SetActive(true);
             planeFinder.SetActive(false);
         }
     }
